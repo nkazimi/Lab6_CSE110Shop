973ce857a43ef1166c730fb7f92f84a68f1665b8
Navid kazimi <nkaz5621@gmail.com>
2021-02-13 23:43:26 -0800

    CSE 110 Shop

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,34 @@
-// Script.js
-
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
-});
+  const itemsInCart = window.localStorage.getItem('itemsInCart')
+
+  if (!itemsInCart) {
+    window.localStorage.setItem('itemsInCart', JSON.stringify([]))
+  }
+
+  const defaultItems = window.localStorage.getItem('productList')
+  let productList = JSON.parse(defaultItems)
+
+  if (!productList) {
+    fetch('https://fakestoreapi.com/products')
+      .then((responseData) => responseData.json())
+      .then((products) => {
+        renderProducts(products)
+        window.localStorage.setItem('productList', JSON.stringify(products))
+      })
+  } else {
+    renderProducts(productList)
+  }
+
+  function renderProducts(productList) {
+    const productListContainer = document.getElementById('product-list')
+    productList.forEach((product) => {
+      const {title, price, description, image, id} = product
+      const productItem = `<product-item title="${title}" price="${price}" img="${image}" alt="${description}" key="${id}" />`
+      productListContainer.insertAdjacentHTML('beforeend', productItem)
+    })
+  }
+
+  // const myStorage = window.localStorage
+  // myStorage.getItem()
+  // myStorage.setItem('productList', JSON.stringify(productList))
+})
